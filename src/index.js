d066dbe981d972e0dfd6be2ccddb474e2a0e567f
gilbertjacke <gilbertjacke@gmail.com>
2017-07-10 22:52:49 +0100

    adjusted bootstrap path

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import '../node_modules/boostrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import configureStore from './store/configureStore';
 
@@ -13,7 +13,7 @@ const store = configureStore();
 
 render(
   <Provider store={store}>
-  <Router routes={routes} history={browserHistory} />,
-</Provider>,
+    <Router routes={routes} history={browserHistory} />,
+  </Provider>,
   document.getElementById('app')
 );
